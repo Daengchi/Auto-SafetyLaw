b97ab4ff29169309bdbb080aa243bcbfec04e617
김용희 <kyhbrad@gmail.com>
2026-06-12 11:25:17 +0900

    설치bat 수정

--- 설치.bat
+++ 설치.bat
@@ -1,3 +1,4 @@
 @echo off
 curl -L -o "%TEMP%\setup.ps1" "https://raw.githubusercontent.com/Daengchi/Auto-SafetyLaw/master/setup.ps1"
 PowerShell -ExecutionPolicy Bypass -File "%TEMP%\setup.ps1"
+pause
